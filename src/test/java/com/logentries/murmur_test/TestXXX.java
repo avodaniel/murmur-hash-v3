2af02c923b74d181d048caf3666c33f326c84b4f
Daniel Fiala <danfiala@ucw.cz>
2012-11-06 12:48:21 +0100

    Added tests for byte functions.

--- src/test/java/com/logentries/murmur_test/TestXXX.java
+++ src/test/java/com/logentries/murmur_test/TestXXX.java
@@ -1,15 +1,13 @@
 package com.logentries.re2_test;
 
 import org.junit.Test;
+import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import java.lang.StringBuffer;
-
-//import com.logentries.murmur.MurmurHashV3;
-//import ie.ucd.murmur.MurmurHashV3;
 
 public class TestXXX {
     static String getXXX(final int len) {
@@ -21,19 +19,28 @@ public class TestXXX {
     }
 
     @Test
-    public void testThreads() {
+    public void testXXX() {
         final String[] xxx = {"xxx",
                               "xxxxxxxxxxxxxxxxxx",
                               "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
                               getXXX(256*1024),
-                              getXXX(256*1024*1024)};
+                              getXXX(128*1024*1024)};
 
         for (int i = 0; i < xxx.length; ++i) {
             final long hash1 = com.logentries.murmur.MurmurHashV3.hash128_64(xxx[i]);
             final long hash2 = ie.ucd.murmur.MurmurHashV3.hash128_64(xxx[i]);
-            System.err.println("i=" + i + "\thash1=" + hash1 + "; hash2=" + hash2);
+            System.err.println("Str.   i=" + i + "\thash1=" + hash1 + "; hash2=" + hash2);
 
             assertEquals(hash1, hash2);
+        }
+
+        for (int i = 0; i < xxx.length; ++i) {
+            final byte[] xxx_bytes = xxx[i].getBytes();
+            final long[] hashes1 = com.logentries.murmur.MurmurHashV3.hash128(xxx_bytes, xxx_bytes.length);
+            final long[] hashes2 = ie.ucd.murmur.MurmurHashV3.hash128(xxx_bytes, xxx_bytes.length);
+            System.err.println("Bytes. i=" + i + "\thash1=" + Arrays.toString(hashes1) + "; hash2=" + Arrays.toString(hashes2));
+
+            assertTrue(Arrays.equals(hashes1, hashes2));
         }
     }
 }
